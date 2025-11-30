e87fda3a1f644b305ac0ef37a18f0cfb1f851028
Th0m4sma <thomaspirescorreia@gmail.com.br>
2025-11-30 16:23:26 -0300

    Validacação Gdrive - Funcções

---
+++ Api/teste.py
@@ -0,0 +1,18 @@
+from jose import jwt
+import os
+from dotenv import load_dotenv
+from Api.gdrive import upload_or_update, find_file_by_name
+
+load_dotenv()
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+jwt_token = os.getenv("JWT_TOKEN")
+
+
+data = jwt.decode(jwt_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+access_token = data["google_access_token"]
+
+# Testa criando/atualizando arquivo
+resultado = upload_or_update(access_token, "teste.json", '{"teste":123}')
+print(resultado)
